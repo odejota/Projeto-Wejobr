6dab6b029bf7bb6bfdc924d0ba3b78893dee77a5
carlapeloia <capeloia@hotmail.com>
2022-03-19 14:10:52 -0300

    Back - classes Candidato + apllicationProperties

---
+++ Projeto Wejobr/wejobr-back/src/main/java/com/wejobr/app/controllers/CandidatoController.java
@@ -0,0 +1,52 @@
+package com.wejobr.app.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wejobr.app.entities.Candidato;
+import com.wejobr.app.services.CandidatoService;
+
+@RestController
+@RequestMapping ("/candidatos")
+//@CrossOrigin(origins = "http://localhost:3000")
+public class CandidatoController {
+	
+	@Autowired
+	private CandidatoService service;
+	
+	@GetMapping
+	public List<Candidato> select() {
+		return service.select();
+	}
+	
+	@GetMapping("/{IdCandidato}")
+	public Candidato selectedById(@PathVariable Long IdCandidato) {
+		return service.selectById(IdCandidato).get();
+	}
+
+	@PostMapping("/admin/cadastrarCandidato")
+	public Candidato insert(@RequestBody Candidato candidato) {
+		return service.insert(candidato);
+	}
+
+	@DeleteMapping("/admin/{IdCandidato}")
+	public void delete(@PathVariable Long IdCandidato) {
+		service.delete(IdCandidato);
+	}
+	
+	@PutMapping("/admin/editarCandidato")
+	public void update(@RequestBody Candidato candidato) {
+		service.update(candidato);
+}
+
+}
---
+++ Projeto Wejobr/wejobr-back/src/main/java/com/wejobr/app/services/CandidatoService.java
@@ -0,0 +1,44 @@
+package com.wejobr.app.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wejobr.app.entities.Candidato;
+import com.wejobr.app.repositories.CandidatoRepository;
+
+@Service
+public class CandidatoService {
+	
+	@Autowired	
+	private CandidatoRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Candidato> select() {
+		return repository.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public Optional<Candidato> selectById(Long IdCandidato) {
+		return repository.findById(IdCandidato);
+	}
+	
+	@Transactional(readOnly = true)
+	public Candidato insert(Candidato candidato) {
+		return repository.save(candidato);
+	}
+
+	@Transactional(readOnly = false)
+	public void delete(Long IdCandidato) {
+		repository.deleteById(IdCandidato);
+	}
+	
+	@Transactional(readOnly = false)
+	public void update(Candidato candidato) {
+		repository.save(candidato);
+	}
+
+}
